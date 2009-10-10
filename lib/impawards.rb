06a02c4446e80e618e4cb8dd306ad323f35815e2
Jon Maddox <jon@jonsthoughtsoneverything.com>
2009-10-09 22:33:37 -0400

    initial

---
+++ lib/impawards.rb
@@ -0,0 +1,60 @@
+require 'rubygems'
+require 'hpricot'
+require 'open-uri'
+require 'pathname'
+require 'cgi'
+
+module IMPAwards
+  class IMPAwards
+
+    def self.search_posters(query)
+      results_url = "http://www.google.com/cse?cx=partner-pub-6811780361519631%3A48v46vdqqnk&cof=FORID%3A9&ie=ISO-8859-1&q=#{CGI.escape(query)}&sa=Search&ad=w9&num=10&rurl=http%3A%2F%2Fwww.impawards.com%2Fgooglesearch.html%3Fcx%3Dpartner-pub-6811780361519631%253A48v46vdqqnk%26cof%3DFORID%253A9%26ie%3DISO-8859-1%26q%3Doverboard%2B1987%26sa%3DSearch"
+      doc = Hpricot open(results_url)
+      movie_urls = (doc/"div.g h2.r a")
+
+      movie_urls.delete_if{|movie_url| movie_url.inner_text !~ /Poster - Internet/i}
+
+      if movie_urls.size > 0
+        posters_for_url(movie_urls.first['href'])
+      else
+        []
+      end
+    end
+
+    def self.posters_for_url(url)
+      posters = []
+      url_path = Pathname.new(url)
+
+      doc = Hpricot markup_for_movie_url(url)
+
+      (doc/"#altdesigns img").each do |element|
+        posters << get_poster_hash(url_path.dirname.to_s + '/' + element['src'])
+      end
+
+      # if no posters were found at the bottom use the one on the page
+      posters << get_poster_hash(url_path.dirname.to_s + '/' + doc.at("#left_half img")['src']) if posters.size == 0
+      posters
+    end
+
+    private
+    def self.markup_for_movie_url(url)
+      markup = open(url).read
+      if markup.match(/URL=\.\.(\/.*\/.*.html)/)
+        markup_for_movie_url("http://impawards.com#{$1}")
+      else
+        markup
+      end
+    end
+
+    def self.get_poster_hash(poster_path)
+      tiny_path = poster_path
+      thumb_path = poster_path.gsub('thumbs', 'posters').gsub('imp_', '')
+      xlg_path = poster_path.gsub('thumbs', 'posters').gsub('imp_', '').gsub('.jpg', '_xlg.jpg')
+
+      poster_hash = { :tiny => tiny_path, 
+                      :thumb => thumb_path, 
+                      :xlg => xlg_path}
+    end
+  end
+end
+
--- test/impawards_test.rb
+++ test/impawards_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class ImpawardsTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  context "IMPAwards" do
+    
+    context "searching for posters by title" do
+      setup do
+        @results = IMPAwards::IMPAwards.search_posters("The Dark Knight")
+      end
+      
+      should "return an array" do
+        assert_equal Array, @results.class
+      end
+
+      should "return an array of hashes" do
+        assert_equal Hash, @results.first.class
+      end
+      
+      context "returned poster" do
+        setup do
+          @result = @results.first
+        end
+
+        should "have 3 sizes" do
+          assert_equal 3, @result.keys.size
+        end
+
+        should "have xlg url" do
+          assert_match /http.*xlg/, @result[:xlg]
+        end
+
+        should "have thumb url" do
+          assert_match /http.*/, @result[:thumb]
+        end
+
+        should "have tiny url" do
+          assert_match /http.*/, @result[:tiny]
+        end
+
+      end
+      
+
+      
+    end
   end
 end
