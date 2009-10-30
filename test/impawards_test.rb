2603fc3910d7e9a087f4c5bc040275930dd37a10
Jon Maddox <jon@jonsthoughtsoneverything.com>
2009-10-30 14:52:32 -0400

    get_posters url requires that the result has the exact query in its title

--- test/impawards_test.rb
+++ test/impawards_test.rb
@@ -5,13 +5,13 @@ class ImpawardsTest < Test::Unit::TestCase
     
     context "searching for posters by title" do
       setup do
-        @results = IMPAwards::IMPAwards.search_posters("The Dark Knight")
+        @results = IMPAwards::IMPAwards.search_posters("K-11")
       end
       
       should "return an array" do
         assert_equal Array, @results.class
       end
-
+    
       should "return an array of hashes" do
         assert_equal Hash, @results.first.class
       end
@@ -20,27 +20,83 @@ class ImpawardsTest < Test::Unit::TestCase
         setup do
           @result = @results.first
         end
-
+    
         should "have 3 sizes" do
           assert_equal 3, @result.keys.size
         end
-
+    
         should "have xlg url" do
           assert_match /http.*xlg/, @result[:xlg]
         end
-
+    
         should "have thumb url" do
           assert_match /http.*/, @result[:thumb]
         end
-
+    
         should "have tiny url" do
           assert_match /http.*/, @result[:tiny]
+        end
+    
+      end
+    end
+
+    context "GETTING for posters for exact title" do
+      
+      context "with good result" do
+        setup do
+          @results = IMPAwards::IMPAwards.get_posters("The Dark Knight")
+        end
+        
+        should "return an array" do
+          assert_equal Array, @results.class
+        end
+      
+        should "return an array of hashes" do
+          assert_equal Hash, @results.first.class
+        end
+      
+        context "returned poster" do
+          setup do
+            @result = @results.first
+          end
+      
+          should "have 3 sizes" do
+            assert_equal 3, @result.keys.size
+          end
+      
+          should "have xlg url" do
+            assert_match /http.*xlg/, @result[:xlg]
+          end
+      
+          should "have thumb url" do
+            assert_match /http.*/, @result[:thumb]
+          end
+      
+          should "have tiny url" do
+            assert_match /http.*/, @result[:tiny]
+          end
+      
+        end
+      end
+      
+      context "with bad results" do
+        setup do
+          @results = IMPAwards::IMPAwards.get_posters("k-11")
+        end
+        
+        should "return an array" do
+          assert_equal Array, @results.class
+        end
+
+        should "return an empty array" do
+          assert @results.empty?
         end
 
       end
       
 
-      
     end
+    
+    
   end
 end
